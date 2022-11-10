a403d7b82d6cec3805070e06b5ff426ecc54592d
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2022-11-10 09:03:43 +0530

    Updated the User controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
@@ -15,9 +17,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "Rumesha Chathumini";
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
     }
 
     @PostMapping("/saveUser")
@@ -26,12 +28,18 @@ public class UserController {
     }
 
     @PutMapping("/updateUser")
-    public String updateUser(){
-        return "User update";
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO) ;
     }
 
     @DeleteMapping("/deleteUser")
-    public String deleteUser(){
-        return "User deleted";
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
     }
+
+    @GetMapping( "/getUserByUserId/{userID}")
+    public UserDTO getUserByUserID(@PathVariable String userID){
+        return userService.getUserByUserID(userID);
+    }
+
 }
